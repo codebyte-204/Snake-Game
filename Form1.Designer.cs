6cf07a48ef94bb4a2fd712c7a4061b21aeee8513
codebyte-204 <codebyte957@gmail.com>
2025-12-14 19:44:35 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.gameOverPanel = new System.Windows.Forms.Panel();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.startPanel = new System.Windows.Forms.Panel();
+            this.comboSpeed = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblHighScore = new System.Windows.Forms.Label();
+            this.tmrGameTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.gamePanel.SuspendLayout();
+            this.gameOverPanel.SuspendLayout();
+            this.startPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.White;
+            this.gamePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gamePanel.Controls.Add(this.gameOverPanel);
+            this.gamePanel.Controls.Add(this.startPanel);
+            this.gamePanel.Location = new System.Drawing.Point(3, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(770, 621);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.GamePanel_Paint);
+            // 
+            // gameOverPanel
+            // 
+            this.gameOverPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.gameOverPanel.Controls.Add(this.btnRestart);
+            this.gameOverPanel.Controls.Add(this.lblMessage);
+            this.gameOverPanel.Location = new System.Drawing.Point(82, 430);
+            this.gameOverPanel.Name = "gameOverPanel";
+            this.gameOverPanel.Size = new System.Drawing.Size(578, 120);
+            this.gameOverPanel.TabIndex = 1;
+            this.gameOverPanel.Visible = false;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(240, 54);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(162, 52);
+            this.btnRestart.TabIndex = 1;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(234, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(168, 32);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "Game Over";
+            // 
+            // startPanel
+            // 
+            this.startPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.startPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.startPanel.Controls.Add(this.comboSpeed);
+            this.startPanel.Controls.Add(this.btnStart);
+            this.startPanel.Controls.Add(this.lblTitle);
+            this.startPanel.Location = new System.Drawing.Point(82, 82);
+            this.startPanel.Name = "startPanel";
+            this.startPanel.Size = new System.Drawing.Size(578, 289);
+            this.startPanel.TabIndex = 0;
+            // 
+            // comboSpeed
+            // 
+            this.comboSpeed.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.comboSpeed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboSpeed.FormattingEnabled = true;
+            this.comboSpeed.Items.AddRange(new object[] {
+            "Easy",
+            "Normal",
+            "Hard"});
+            this.comboSpeed.Location = new System.Drawing.Point(197, 175);
+            this.comboSpeed.Name = "comboSpeed";
+            this.comboSpeed.Size = new System.Drawing.Size(172, 28);
+            this.comboSpeed.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Silver;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(197, 102);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(173, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(191, 31);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(189, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Snake Game";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.LightCoral;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(793, 265);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(100, 25);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score : 0";
+            // 
+            // lblHighScore
+            // 
+            this.lblHighScore.AutoSize = true;
+            this.lblHighScore.BackColor = System.Drawing.Color.LightCoral;
+            this.lblHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighScore.Location = new System.Drawing.Point(779, 216);
+            this.lblHighScore.Name = "lblHighScore";
+            this.lblHighScore.Size = new System.Drawing.Size(118, 20);
+            this.lblHighScore.TabIndex = 2;
+            this.lblHighScore.Text = "High Score: 0";
+            // 
+            // tmrGameTimer
+            // 
+            this.tmrGameTimer.Interval = 150;
+            this.tmrGameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(0, 0);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 645);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblHighScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.gamePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.gamePanel.ResumeLayout(false);
+            this.gameOverPanel.ResumeLayout(false);
+            this.gameOverPanel.PerformLayout();
+            this.startPanel.ResumeLayout(false);
+            this.startPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Panel startPanel;
+        private System.Windows.Forms.ComboBox comboSpeed;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel gameOverPanel;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblHighScore;
+        private System.Windows.Forms.Timer tmrGameTimer;
+        private System.Windows.Forms.Button btnPause;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+        public partial class Form1 : Form
+        {
+        // ===== GRID =====
+        private const int gridWidth = 25;
+        private const int gridHeight = 20;
+        private const int cellSize = 20;
+
+        // ===== GAME STATE =====
+        private enum GameState { Start, Running }
+        private GameState gameState = GameState.Start;
+
+        private enum Direction { Up, Down, Left, Right }
+        private Direction currentDirection = Direction.Right;
+        private Direction nextDirection = Direction.Right;
+
+        private enum Difficulty { Easy, Normal, Hard }
+        private Difficulty currentDifficulty = Difficulty.Easy;
+
+        // ===== GAME DATA =====
+        private List<Point> snake = new List<Point>();
+        private List<Point> obstacles = new List<Point>();
+        private Point food;
+        private Point bonusFood;
+
+        private bool hasBonus = false;
+        private int bonusTimer = 0;
+        private const int BONUS_LIFETIME = 50;
+
+        private int foodEatenCount = 0;
+        private int score = 0;
+        private int highScore = 0;
+
+        private Random rnd = new Random();
+
+        // ===== FILE =====
+        private readonly string scoreFilePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "MySnakeGame", "highscore.txt");
+
+        // ===== SOUND =====
+        SoundPlayer sndEat = new SoundPlayer(Properties.Resources.eat);
+        SoundPlayer sndBonus = new SoundPlayer(Properties.Resources.bonus);
+        SoundPlayer sndGameOver = new SoundPlayer(Properties.Resources.gameover);
+        SoundPlayer sndClick = new SoundPlayer(Properties.Resources.click);
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+
+            // Game panel size
+            gamePanel.Width = gridWidth * cellSize;
+            gamePanel.Height = gridHeight * cellSize;
+
+            typeof(Panel).GetProperty("DoubleBuffered",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .SetValue(gamePanel, true, null);
+
+            // Timer
+            tmrGameTimer.Interval = 150;
+            tmrGameTimer.Tick += GameTimer_Tick;
+
+            // Events
+            gamePanel.Paint += GamePanel_Paint;
+            KeyDown += Form1_KeyDown;
+            btnStart.Click += BtnStart_Click;
+            btnRestart.Click += BtnRestart_Click;
+
+            LoadHighScore();
+
+            startPanel.Visible = true;
+            gameOverPanel.Visible = false;
+        }
+
+        // ===== START GAME =====
+        private void StartGame()
+        {
+            sndClick.Play();
+
+            snake.Clear();
+            obstacles.Clear();
+
+            snake.Add(new Point(gridWidth / 2, gridHeight / 2));
+            currentDirection = nextDirection = Direction.Right;
+
+            score = 0;
+            foodEatenCount = 0;
+            hasBonus = false;
+
+            DetectDifficulty();
+            ApplyDifficulty();
+            GenerateFood();
+
+            UpdateLabels();
+
+            startPanel.Visible = false;
+            gameOverPanel.Visible = false;
+
+            gameState = GameState.Running;
+            tmrGameTimer.Start();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e) => StartGame();
+        private void BtnRestart_Click(object sender, EventArgs e) => StartGame();
+
+        // ===== KEY CONTROL =====
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState == GameState.Start && e.KeyCode == Keys.Enter)
+            {
+                StartGame();
+                return;
+            }
+
+            if (gameState != GameState.Running) return;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up: if (currentDirection != Direction.Down) nextDirection = Direction.Up; break;
+                case Keys.Down: if (currentDirection != Direction.Up) nextDirection = Direction.Down; break;
+                case Keys.Left: if (currentDirection != Direction.Right) nextDirection = Direction.Left; break;
+                case Keys.Right: if (currentDirection != Direction.Left) nextDirection = Direction.Right; break;
+            }
+        }
+
+        // ===== DIFFICULTY =====
+        private void DetectDifficulty()
+        {
+            switch (comboSpeed.SelectedItem?.ToString())
+            {
+                case "Normal": currentDifficulty = Difficulty.Normal; break;
+                case "Hard": currentDifficulty = Difficulty.Hard; break;
+                default: currentDifficulty = Difficulty.Easy; break;
+            }
+        }
+
+        private void ApplyDifficulty()
+        {
+            obstacles.Clear();
+
+            switch (currentDifficulty)
+            {
+                case Difficulty.Easy:
+                    tmrGameTimer.Interval = 220;
+                    break;
+
+                case Difficulty.Normal:
+                    tmrGameTimer.Interval = 150;
+                    AddRandomObstacles(6);
+                    break;
+
+                case Difficulty.Hard:
+                    tmrGameTimer.Interval = 100;
+                    GenerateMaze();
+                    break;
+            }
+        }
+
+        private void AddRandomObstacles(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Point p;
+                do
+                {
+                    p = new Point(rnd.Next(1, gridWidth - 1), rnd.Next(1, gridHeight - 1));
+                }
+                while (snake.Contains(p) || obstacles.Contains(p));
+
+                obstacles.Add(p);
+            }
+        }
+
+        private void GenerateMaze()
+        {
+            for (int y = 2; y < gridHeight - 2; y += 3)
+                for (int x = 2; x < gridWidth - 2; x++)
+                    if (x % 4 == 0)
+                        obstacles.Add(new Point(x, y));
+        }
+
+        // ===== GAME LOOP =====
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            if (gameState != GameState.Running) return;
+            MoveSnake();
+            gamePanel.Invalidate();
+        }
+
+        private void MoveSnake()
+        {
+            currentDirection = nextDirection;
+            Point head = snake[0];
+            Point newHead = head;
+
+            if (currentDirection == Direction.Up) newHead.Y--;
+            if (currentDirection == Direction.Down) newHead.Y++;
+            if (currentDirection == Direction.Left) newHead.X--;
+            if (currentDirection == Direction.Right) newHead.X++;
+
+            if (newHead.X < 0 || newHead.X >= gridWidth ||
+                newHead.Y < 0 || newHead.Y >= gridHeight ||
+                snake.Contains(newHead) || obstacles.Contains(newHead))
+            {
+                EndGame();
+                return;
+            }
+
+            snake.Insert(0, newHead);
+
+            if (newHead == food)
+            {
+                sndEat.Play();
+                score++;
+                foodEatenCount++;
+
+                if (foodEatenCount % 3 == 0)
+                    SpawnBonus();
+
+                GenerateFood();
+            }
+            else if (hasBonus && newHead == bonusFood)
+            {
+                sndBonus.Play();
+                score += 5;
+                hasBonus = false;
+            }
+            else
+            {
+                snake.RemoveAt(snake.Count - 1);
+            }
+
+            if (hasBonus && --bonusTimer <= 0)
+                hasBonus = false;
+
+            UpdateLabels();
+        }
+
+        // ===== FOOD =====
+        private void GenerateFood()
+        {
+            Point p;
+            do
+            {
+                p = new Point(rnd.Next(0, gridWidth), rnd.Next(0, gridHeight));
+            }
+            while (snake.Contains(p) || obstacles.Contains(p));
+
+            food = p;
+        }
+
+        private void SpawnBonus()
+        {
+            Point p;
+            do
+            {
+                p = new Point(rnd.Next(0, gridWidth), rnd.Next(0, gridHeight));
+            }
+            while (snake.Contains(p) || obstacles.Contains(p));
+
+            bonusFood = p;
+            hasBonus = true;
+            bonusTimer = BONUS_LIFETIME;
+        }
+
+        // ===== DRAW =====
+        private void GamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.FromArgb(20, 20, 30));
+
+            Pen gridPen = new Pen(Color.FromArgb(40, 0, 255, 255));
+            for (int x = 0; x <= gridWidth; x++)
+                g.DrawLine(gridPen, x * cellSize, 0, x * cellSize, gridHeight * cellSize);
+            for (int y = 0; y <= gridHeight; y++)
+                g.DrawLine(gridPen, 0, y * cellSize, gridWidth * cellSize, y * cellSize);
+
+            g.DrawRectangle(Pens.Cyan, 0, 0, gridWidth * cellSize - 1, gridHeight * cellSize - 1);
+
+            foreach (var o in obstacles)
+                g.FillRectangle(Brushes.DarkRed, o.X * cellSize, o.Y * cellSize, cellSize, cellSize);
+
+            g.FillEllipse(Brushes.Red,
+                food.X * cellSize + 2, food.Y * cellSize + 2,
+                cellSize - 4, cellSize - 4);
+
+            if (hasBonus)
+                g.FillEllipse(Brushes.Gold,
+                    bonusFood.X * cellSize + 2, bonusFood.Y * cellSize + 2,
+                    cellSize - 4, cellSize - 4);
+
+            for (int i = 0; i < snake.Count; i++)
+            {
+                Brush b = i == 0 ? Brushes.Lime : Brushes.Green;
+                g.FillRectangle(b,
+                    snake[i].X * cellSize,
+                    snake[i].Y * cellSize,
+                    cellSize, cellSize);
+            }
+
+            if (hasBonus)
+            {
+                int w = (int)((bonusTimer / (float)BONUS_LIFETIME) * gamePanel.Width);
+                g.FillRectangle(Brushes.Gold, 0, gamePanel.Height - 5, w, 5);
+            }
+        }
+
+        // ===== GAME OVER =====
+        private void EndGame()
+        {
+            sndGameOver.Play();
+            tmrGameTimer.Stop();
+            gameState = GameState.Start;
+
+            if (score > highScore)
+            {
+                highScore = score;
+                SaveHighScore();
+            }
+
+            lblHighScore.Text = $"High Score: {highScore}";
+            MessageBox.Show($"Game Over!\nYour Score: {score}",
+                "Snake Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            startPanel.Visible = true;
+        }
+
+        // ===== SCORE =====
+        private void UpdateLabels()
+        {
+            lblScore.Text = $"Score: {score}";
+            lblHighScore.Text = $"High Score: {highScore}";
+        }
+
+        private void LoadHighScore()
+        {
+            if (File.Exists(scoreFilePath))
+                int.TryParse(File.ReadAllText(scoreFilePath), out highScore);
+        }
+
+        private void SaveHighScore()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(scoreFilePath));
+            File.WriteAllText(scoreFilePath, highScore.ToString());
+        }
+
+   
+    }
+}
+
+
